df34dda9a3d1959510a6f565be12782b2c11c39d
Balraj Patel <balrajpatel06@gmail.com>
2026-02-13 11:28:08 +0530

    Initialize Spring boot backend project structure

---
+++ backend/src/test/java/com/smarthome/energy/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smarthome.energy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
