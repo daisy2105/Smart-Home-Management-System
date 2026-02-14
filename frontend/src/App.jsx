fe50bf34eba9a22f7f303f08d875428140bd2d77
Rahulwankar <rahulvankar1534@gmail.com>
2026-02-14 16:36:42 +0530

    Added Routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import LandingPageRoute from './Routes/AppRoutes';
 
 const App = () => {
+
   return (
-    <div>App</div>
+     <div>
+        <LandingPageRoute/>
+    </div>
   )
 }
 
