9c6355ba8f0c57464e03d07092f72b8ee0c91069
RahulWankar <rahulvankar1534@gmail.com>
2026-03-29 16:06:13 +0530

    Fixed Energy Usage related Context and API

--- frontend/src/context/EnergyContext.jsx
+++ frontend/src/context/EnergyContext.jsx
@@ -1,81 +1,48 @@
 import React, { createContext, useContext, useEffect, useState } from "react";
-import { getDailyConsumption, getMonthlyConsumption } from "../service/energyService";
+
+import { getTodayEnergyConsumption, getCurrentMonthEnergyConsumption } from "../service/energyService";
 import { UserContext } from "./UserContext";
-import { DeviceContext } from "./DeviceContext";
 
 export const EnergyContext = createContext();
 
 const EnergyProvider = ({ children }) => {
   const { UserDetail } = useContext(UserContext);
-  const { devices } = useContext(DeviceContext);
+
+  //  user login check
+  const isLoggedIn = !!UserDetail?.role;
 
   const [todayEnergy, setTodayEnergy] = useState(0);
   const [monthlyEnergy, setMonthlyEnergy] = useState(0);
+  const [loading, setLoading] = useState(false);
 
-  const isLoggedIn = !!UserDetail?.role;
+  /* FETCH ENERGY DATA */
+  const fetchEnergyUsage = async () => {
+    try {
+      setLoading(true);
 
-  const calculateTodayEnergy = (dailyData) => {              // Calculate Current Day Energy
-    const today = new Date();
-    let energy = 0;
+      // ENERGY
+      const todayEnergyRes = await getTodayEnergyConsumption();
+      const monthEnergyRes = await getCurrentMonthEnergyConsumption();
 
-    dailyData?.forEach((item) => {                          // Fetch All Devices and Check Current Day 
-      if (
-        item.year === today.getFullYear() &&
-        item.month === today.getMonth() + 1 &&
-        item.day === today.getDate()
-      ) {
-        energy = item.energyConsumption || 0;
-      }
-    });
+      setTodayEnergy(Number(todayEnergyRes.toFixed(2)));             // only 2 decimal after point
+      setMonthlyEnergy(Number(monthEnergyRes.toFixed(2)));
 
-    return Number(energy.toFixed(2));                         // Geeting Upto 2 Decimal Number (228.55)
+    } catch (error) {
+      console.error("Energy Context Error:", error);
+    } finally {
+      setLoading(false);
+    }
   };
 
-  const calculateMonthlyEnergy = (monthlyData) => {            // Calculate Monthly Energy
-    const today = new Date();
-    let energy = 0;
+    /* LOAD WHEN USER LOGS IN */
+    useEffect(() => {
+      if (!isLoggedIn) return;
 
-    monthlyData?.forEach((item) => {                          // Fetch All Devices and Check Current Month 
-      if (
-        item.year === today.getFullYear() &&
-        item.month === today.getMonth() + 1
-      ) {
-        energy = item.energyConsumption || 0;
-      }
-    });
-
-    return Number(energy.toFixed(2));                         // Geeting Upto 2 Decimal Number (228.55)
-  };
-
-  useEffect(() => {                                            // Fetch Data
-    if (!isLoggedIn) return;
-
-    const fetchEnergy = async () => {
-      try {
-        const today = new Date();                               // Current Day
-        const year = today.getFullYear();                       // Current year
-        const month = today.getMonth() + 1;                     // Current Month by (31 or 30 Days)
-
-        const dailyData = await getDailyConsumption(year, month);               // Send Data to Backend
-        const monthlyData = await getMonthlyConsumption(year);
-
-        setTodayEnergy(calculateTodayEnergy(dailyData));
-        setMonthlyEnergy(calculateMonthlyEnergy(monthlyData));
-
-      } catch (error) {
-        console.error("Energy fetch failed:", error);
-      }
-    };
-
-    fetchEnergy();
-
-    const timer = setInterval(fetchEnergy, 1000);               // Call Every 1sec
-
-    return () => clearInterval(timer);                          // Clear
-  }, [isLoggedIn, devices]);
+      fetchEnergyUsage();
+    }, [isLoggedIn]);
 
   return (
-    <EnergyContext.Provider value={{ todayEnergy, monthlyEnergy }}>
+    <EnergyContext.Provider value={{ todayEnergy, monthlyEnergy, loading }}>
       {children}
     </EnergyContext.Provider>
   );
